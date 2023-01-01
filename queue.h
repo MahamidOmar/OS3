3528e4248bed0e00f158f0cdad06220173a75a3e
AymanBarham <103962614+AymanBarham@users.noreply.github.com>
2023-01-01 16:24:10 +0200

    0.02 queue finished

---
+++ queue.h
@@ -0,0 +1,27 @@
+//
+// Created by Ayman Barham on 01/01/2023.
+//
+
+#ifndef OS3_QUEUE_H
+#define OS3_QUEUE_H
+
+#include "stdbool.h"
+#include "segel.h"
+
+typedef struct queue* Queue;
+typedef void* Element;
+typedef Element (*copyElement)(Element);
+typedef void (*freeElement)(Element);
+
+Queue createQueue(unsigned int maxSize, copyElement copyFunc, freeElement freeFunc);
+void freeQueue(Queue q);
+bool isEmptyQueue(Queue q);
+//bool isFullQueue(Queue);
+unsigned int getSizeQueue(Queue q);
+void addElement(Queue q, Element value);
+void dequeElement(Queue q);
+Element topElement(Queue q);
+Queue removeHalfElementsRandomly(Queue q, Queue deletedNodes);
+
+
+#endif //OS3_QUEUE_H
