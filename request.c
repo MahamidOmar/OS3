acd603cd83feba6f774ea78fc832f22c6f8471c0
AymanBarham <103962614+AymanBarham@users.noreply.github.com>
2023-01-03 15:29:51 +0200

    0.14  testing reverted print

--- request.c
+++ request.c
@@ -195,7 +195,7 @@ void requestServeStatic(Request req, char *filename, int filesize)
     sprintf(buf, "HTTP/1.0 200 OK\r\n");
     sprintf(buf, "%sServer: OS-HW3 Web Server\r\n", buf);
     sprintf(buf, "%sContent-Length: %d\r\n", buf, filesize);
-    sprintf(buf, "%sContent-Type: %s\r\n", buf, filetype);
+    sprintf(buf, "%sContent-Type: %s\r\n\r\n", buf, filetype);
 
     sprintf(buf, "%sStat-Req-Arrival:: %lu.%06lu\r\n", buf, req->arrive_time->tv_sec, req->arrive_time->tv_usec);
     sprintf(buf, "%sStat-Req-Dispatch:: %lu.%06lu\r\n", buf, req->dispatch_time->tv_sec, req->dispatch_time->tv_usec);
