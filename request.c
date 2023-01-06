ea4c9e4acf7741424cc788c551e2e0604b2482d0
AymanBarham <103962614+AymanBarham@users.noreply.github.com>
2023-01-06 21:47:13 +0200

    0.38  testing makefile fix8

--- request.c
+++ request.c
@@ -29,39 +29,39 @@ void requestError(Request req, char *cause, char *errnum, char *shortmsg, char *
     // Write out the header information for this response
     sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
     Rio_writen(fd, buf, strlen(buf));
-    sprintf(buf, "%s");
+    printf("%s", buf);
 
     sprintf(buf, "Stat-Req-Arrival:: %lu.%06lu\r\n", req->arrive_time->tv_sec, req->arrive_time->tv_usec);
     Rio_writen(fd, buf, strlen(buf));
-    sprintf(buf, "%s");
+    printf("%s", buf);
 
     sprintf(buf, "Stat-Req-Dispatch:: %lu.%06lu\r\n", req->dispatch_time->tv_sec, req->dispatch_time->tv_usec);
     Rio_writen(fd, buf, strlen(buf));
-    sprintf(buf, "%s");
+    printf("%s", buf);
 
     sprintf(buf, "Stat-Thread-Id:: %d\r\n",  getThreadId(req->st));
     Rio_writen(fd, buf, strlen(buf));
-    sprintf(buf, "%s");
+    printf("%s", buf);
 
     sprintf(buf, "Stat-Thread-Count:: %d\r\n", getThreadCount(req->st));
     Rio_writen(fd, buf, strlen(buf));
-    sprintf(buf, "%s");
+    printf("%s", buf);
 
     sprintf(buf, "Stat-Thread-Static:: %d\r\n", getThreadStaticCount(req->st));
     Rio_writen(fd, buf, strlen(buf));
-    sprintf(buf, "%s");
+    printf("%s", buf);
 
     sprintf(buf, "Stat-Thread-Dynamic:: %d\r\n", getThreadDynamicCount(req->st));
     Rio_writen(fd, buf, strlen(buf));
-    sprintf(buf, "%s");
+    printf("%s", buf);
 
     sprintf(buf, "Content-Type: text/html\r\n");
     Rio_writen(fd, buf, strlen(buf));
-    sprintf(buf, "%s");
+    printf("%s", buf);
 
     sprintf(buf, "Content-Length: %lu\r\n\r\n", strlen(body));
     Rio_writen(fd, buf, strlen(buf));
-    sprintf(buf, "%s");
+    printf("%s", buf);
 
     // Write out the content
     Rio_writen(fd, body, strlen(body));
